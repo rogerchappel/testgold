aae2bdaf3d3f77243bfe7b8faa5285768a8f1be2
Roger Chappel <miscanalysis@gmail.com>
2026-05-19 18:37:17 +1000

    feat: export library API

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+export { compareGolden, normalizeFixture } from './golden.js';
+export { loadConfig } from './config.js';
+export { normalizeText } from './normalizers.js';
+export { normalizeJson } from './json.js';
+export { unifiedDiff } from './diff.js';
+export { renderSummary } from './summary.js';
+export type {
+  BuiltInScrubber,
+  CompareOptions,
+  CompareResult,
+  CompareStatus,
+  CompareSummary,
+  JsonMode,
+  ScrubberConfig,
+  TestGoldConfig
+} from './types.js';
